6fd3b3ff1a7f11876915a75adf3d02d1a8ab4373
DorianHaley <59707012+DorianHaley@users.noreply.github.com>
2020-02-07 14:03:39 -0600

    INIT

---
+++ LINQ/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // var words = new List<string>() { " The", " Bike", " This", " Mathmatics", " Tenth", " It" };
+          //  var wordsWithTH = words.Where(w => w.Contains("Th")).ToList();
+
+          //  foreach (var word in wordsWithTH)
+           // {
+          //      Console.WriteLine(word);
+           // }
+           // Console.ReadLine();
+        
+
+          
+        
+            //var names = new List<string>() { " Mike", " Brad", " Nevin", " Ian", " Mike" };
+           // var NonDuplicateName = names.Distinct();
+
+           // foreach (var Name in NonDuplicateName)
+           // {
+           //     Console.WriteLine(Name);
+           // }
+           // Console.ReadLine();
+
+
+        }
+    }
+       
+}
